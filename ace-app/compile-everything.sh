4c33b8f7a72c44ff13c1ec105f4e564e1c53d4ba
Adriaan Lemmens <Adriaan@lemontheme.local>
2016-03-28 15:57:30 +0200

    first working version of entire pipeline

---
+++ ace-app/compile-everything.sh
@@ -0,0 +1,4 @@
+# compile grammars as ACE images
+./ace -G mini-sclera.dat -g ../grammars/mini-sclera/ace/config.tdl;
+./ace -G mini-dutch.dat -g ../grammars/mini-dutch/ace/config.tdl;
+./ace -G transfer-nl.dat -g ../grammars/transfer-nl/ace/config.tdl;
---
+++ ace-app/demo_1.sh
@@ -0,0 +1,1 @@
+echo "dog sleep" | ./ace -g mini-sclera.dat | ./ace -g transfer-nl.dat | ./ace -g mini-dutch.dat -e;
