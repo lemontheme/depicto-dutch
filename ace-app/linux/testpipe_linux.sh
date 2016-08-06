cb87637301a7402627e3b3f1c7b4717de41c554a
Adriaan Lemmens <Adriaan@lemontheme.local>
2016-08-06 21:34:00 +0200

    fixing linux troubles pt4

--- ace-app/linux/testpipe_linux.sh
+++ ace-app/linux/testpipe_linux.sh
@@ -1,2 +1,1 @@
-./ace_linux -g ../mini-sclera_linux.dat | ./ace_linux -g
-../transfer-nl_linux.dat | ./ace_linux -g ../mini-dutch_linux.dat -e
+./ace_linux -g ../mini-sclera_linux.dat | ./ace_linux -g ../transfer-nl_linux.dat | ./ace_linux -g ../mini-dutch_linux.dat -e
