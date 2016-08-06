60057099a0398cbaf635e784421d65b76ace99d4
Adriaan Lemmens <Adriaan@lemontheme.local>
2016-08-06 18:11:44 +0200

    trying to fix ELF header error pt1

--- ace-app/linux/compile-all_linux.sh
+++ ace-app/linux/compile-all_linux.sh
@@ -1,4 +1,4 @@
 # compile grammars as ACE images
-./ace_linux -G mini-sclera.dat -g ../../grammars/mini-sclera/ace/config.tdl;
-./ace_linux -G mini-dutch.dat -g ../../grammars/mini-dutch/ace/config.tdl;
-./ace_linux -G transfer-nl.dat -g ../../grammars/transfer-nl/ace/config.tdl;
+./ace_linux -G ../mini-sclera.dat -g ../../grammars/mini-sclera/ace/config.tdl;
+./ace_linux -G ../mini-dutch.dat -g ../../grammars/mini-dutch/ace/config.tdl;
+./ace_linux -G ../transfer-nl.dat -g ../../grammars/transfer-nl/ace/config.tdl;
